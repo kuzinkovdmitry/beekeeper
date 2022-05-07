eae6dd2d0c52baba798e64e28953e8022ebf9847
Kuzinkov Dmitry <dmytro.kuzinkov@chanty.com>
2022-05-07 15:28:01 +0300

    qwe

--- src/app/data/base-url.ts
+++ src/app/data/base-url.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://751e-87-244-158-133.ngrok.io/';
+export const BASE_URL = 'https://6390-87-244-158-133.ngrok.io/';
--- src/app/pages/plan/plan.component.ts
+++ src/app/pages/plan/plan.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { PagesService } from 'src/app/services/pages.service';
 
 @Component({
@@ -11,15 +10,45 @@ import { PagesService } from 'src/app/services/pages.service';
 export class PlanComponent implements OnInit {
   isLoading = false;
   selectedAlgorithm;
-  algorithms;
+  algorithms = ['Greeady algorithm', 'Borders and branches method'];
+  algoritmResult;
 
   constructor(private pagesService: PagesService,
-              private dialog: MatDialog,
               private cdRef: ChangeDetectorRef) {
-this.pagesService.setPageUrl('plan');
-}
+  this.pagesService.setPageUrl('plan');
+  }
 
   ngOnInit(): void {
+  }
+
+  public getData(): void {
+    this.algoritmResult = null;
+    this.isLoading = true;
+    this.cdRef.detectChanges();
+    setTimeout(() => {
+      this.algoritmResult = [
+        {
+          id: 1,
+          leftForWinter: 110,
+          profit: '220 000',
+          goods: [
+            { name: 'Honey', total: 10 },
+            { name: 'Honeycomb', total: 11 }
+          ]
+        },
+        {
+          id: 2,
+          leftForWinter: 77,
+          profit: '12 309',
+          goods: [
+            { name: 'Honey', total: 4 },
+            { name: 'Honeycomb', total: 21 }
+          ]
+        }
+      ];
+      this.isLoading = false;
+      this.cdRef.detectChanges();
+    }, 1000);
   }
 
 }
